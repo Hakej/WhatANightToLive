7694760422b48c522af069495955ed4729f79da8
Hakej <michaluszak97@gmail.com>
2020-12-01 16:30:59 +0100

    Add win scene

---
+++ Assets/TypeWriterEffect.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypeWriterEffect : MonoBehaviour
+{
+    public float Delay = 0.1f;
+    public Text Text;
+    
+    private string _fullText;
+    private string _currentText = "";
+
+    private void Start()
+    {
+        _fullText = Text.text;
+        StartCoroutine(ShowText());
+    }
+
+    private IEnumerator ShowText()
+    {
+        for (var i = 0; i <= _fullText.Length; i++)
+        {
+            _currentText = _fullText.Substring(0, i);
+            Text.text = _currentText;
+            yield return new WaitForSeconds(Delay);
+        }
+    }
+}
