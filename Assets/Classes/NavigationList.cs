c6dc413b643f21004db1fa95fe31b5849f0023f9
Hakej <michaluszak97@gmail.com>
2020-12-03 12:05:36 +0100

    Add Navigation List

---
+++ Assets/Classes/NavigationList.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace Classes
+{
+    public class NavigationList<T> : List<T>
+    {
+        private int _currentIndex = 0;
+        
+        public int CurrentIndex
+        {
+            get
+            {
+                if (_currentIndex > Count - 1) { _currentIndex = Count - 1; }
+                if (_currentIndex < 0) { _currentIndex = 0; }
+                return _currentIndex;
+            }
+            set { _currentIndex = value; }
+        }
+
+        public T MoveNext
+        {
+            get { _currentIndex++; return this[CurrentIndex]; }
+        }
+
+        public T MovePrevious
+        {
+            get { _currentIndex--; return this[CurrentIndex]; }
+        }
+
+        public T Current
+        {
+            get { return this[CurrentIndex]; }
+        }
+
+        public T GetNext()
+        {
+            _currentIndex++;
+            var next = this[CurrentIndex];
+            _currentIndex--;
+            return next;
+        }
+        
+        public T GetPrevious()
+        {
+            _currentIndex--;
+            var next = this[CurrentIndex];
+            _currentIndex++;
+            return next;
+        }
+    }
+}
