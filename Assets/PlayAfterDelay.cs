5920e8f74e98afdddad3cbb5410afe8281e1a800
Hakej <michaluszak97@gmail.com>
2020-12-06 14:55:00 +0100

    Improve win scene
    Add music and sound effects to win scene

---
+++ Assets/PlayAfterDelay.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayAfterDelay : MonoBehaviour
+{
+    public AudioSource AudioSource;
+    public float Delay;
+
+    private void Start()
+    {
+        AudioSource.PlayDelayed(Delay);
+    }
+}
