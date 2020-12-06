a79387b550960302d23a39b94fa3d63e5e65d6aa
Hakej <michaluszak97@gmail.com>
2020-12-06 15:50:42 +0100

    Add new TV sound
    Adjust powered device script

--- Assets/Scripts/PoweredDevice.cs
+++ Assets/Scripts/PoweredDevice.cs
@@ -1,24 +1,63 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class PoweredDevice : MonoBehaviour
 {
     public GameObject[] GameObjectsToToggle;
 
-    private void Start()
+    public AudioSource PowerSound;
+    public AudioClip PowerOn;
+    public AudioClip PowerRunning;
+    public AudioClip PowerOff;
+
+    public bool IsMuted = true;
+    
+    private void Awake()
     {
         EventHandler.Instance.OnPowerToggle += OnToggle;
     }
 
-    private void OnToggle(bool activity, string poweredTag)
+    private void OnToggle(bool isRunning, string poweredTag)
     {
         if (!CompareTag(poweredTag))
         {
             return;
         }
 
+        if (!IsMuted)
+        {
+            PlayAudio(isRunning);
+        }
+            
         foreach (var component in GameObjectsToToggle)
         {
-            component.gameObject.SetActive(activity);
+            component.gameObject.SetActive(isRunning);
         }
+    }
+
+    private void PlayAudio(bool isRunning)
+    {
+        PowerSound.loop = false;
+        
+        if (isRunning)
+        {
+            PowerSound.clip = PowerOn;
+            PowerSound.Play();
+            StartCoroutine(PlayAfterPowerOn());
+        }
+        else
+        {
+            PowerSound.clip = PowerOff;
+            PowerSound.Play();
+        }
+    }
+    
+    private IEnumerator PlayAfterPowerOn()
+    {
+        yield return new WaitForSeconds(PowerOn.length);
+
+        PowerSound.clip = PowerRunning;
+        PowerSound.loop = true;
+        PowerSound.Play();
     }
 }
