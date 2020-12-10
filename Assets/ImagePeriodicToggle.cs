8b6ebe758008596cbf19d5477050360a12585c10
Hakej <michaluszak97@gmail.com>
2020-12-10 13:54:18 +0100

    Add recording circle in the corner of security camera UI

---
+++ Assets/ImagePeriodicToggle.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ImagePeriodicToggle : MonoBehaviour
+{
+    public Image ImageToToggle;
+    public float Interval;
+
+    private float _currentTime;
+
+    private bool _toggleState;
+
+    private void Start()
+    {
+        _toggleState = ImageToToggle.enabled;
+    }
+
+    private void Update()
+    {
+        _currentTime += Time.deltaTime;
+
+        if (_currentTime < Interval)
+        {
+            return;
+        }
+
+        _currentTime = 0f;
+        _toggleState = !_toggleState;
+        ImageToToggle.enabled = _toggleState;
+    }
+}
