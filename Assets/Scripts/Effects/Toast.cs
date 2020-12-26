fa744e648eb1d958a886a8772fdca30e5c23a8d8
Hakej <michaluszak97@gmail.com>
2020-12-26 13:49:13 +0100

    Project cleanup and refactor

---
+++ Assets/Scripts/Effects/Toast.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Effects
+{
+    public class Toast : Singleton<Toast>
+    {
+        public AudioSource ToastSound;
+        public Canvas Canvas;
+        public TextMeshProUGUI Text;
+
+        public void Show(string message, float time)
+        {
+            ToastSound.Play();
+        
+            Text.text = message;
+            var textObj = Instantiate(Text, Canvas.transform);
+            Destroy(textObj.gameObject, time);
+        }
+    }
+}
