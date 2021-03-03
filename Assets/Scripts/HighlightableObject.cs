e8aacc93408b03e094848080614e82dadc513350
Hakej <michaluszak97@gmail.com>
2021-03-03 12:51:26 +0100

    Add highlight of interactable objects

---
+++ Assets/Scripts/HighlightableObject.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class HighlightableObject : MonoBehaviour
+{
+    public Light[] LightsToToggle;
+    
+    private Renderer[] _renderer;
+
+    private void Start()
+    {
+        _renderer = GetComponentsInChildren<Renderer>();
+    }
+
+    private void OnMouseEnter()
+    {
+        foreach (var renderer in _renderer)
+        {
+            renderer.material.EnableKeyword("_EMISSION");
+        }
+
+        foreach (var light in LightsToToggle)
+        {
+            light.intensity = 0.5f;
+        }
+    }
+
+    private void OnMouseExit()
+    {        
+        foreach (var renderer in _renderer)
+        {
+            renderer.material.DisableKeyword("_EMISSION");
+        }
+        
+        
+        foreach (var light in LightsToToggle)
+        {
+            light.intensity = 0f;
+        }
+    }
+}
