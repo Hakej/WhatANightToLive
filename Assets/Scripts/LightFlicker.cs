3b2ddbe4b6c0d21cc2f0fabd45fa1b9489f873ae
Hakej <michaluszak97@gmail.com>
2021-03-03 13:01:19 +0100

    Add LightFlicker script to flashlight

---
+++ Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class LightFlicker : MonoBehaviour
+{
+    public Light Light;
+    public float LowIntensity;
+    
+    private float _baseIntensity;
+
+    private void Start()
+    {
+        _baseIntensity = Light.intensity;
+    }
+
+    private void Update()
+    {
+        Light.intensity = Random.Range(LowIntensity, _baseIntensity);
+    }
+
+    private void OnDisable()
+    {
+        Light.intensity = _baseIntensity;
+    }
+}
