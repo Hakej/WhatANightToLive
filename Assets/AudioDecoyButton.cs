8bea1719dc611e451944004dab85e537193fe5fe
Hakej <michaluszak97@gmail.com>
2021-03-20 17:26:49 +0100

    Add visual representation of audio decoy playing

---
+++ Assets/AudioDecoyButton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioDecoyButton : MonoBehaviour
+{
+    public AudioDecoy AudioDecoy;
+    public Image Image;
+    public Sprite SpriteDecoyPlaying;
+    public Sprite SpriteDecoyNotPlaying;
+
+    private void Start()
+    {
+        CheckSprite();
+    }
+
+    public void ToggleAudioDecoy()
+    {
+        AudioDecoy.ToggleDecoy();
+        CheckSprite();
+    }
+
+    public void CheckSprite()
+    {
+        if (AudioDecoy.DecoyAudioSource.isPlaying)
+        {
+            Image.sprite = SpriteDecoyPlaying;
+        }
+        else
+        {
+            Image.sprite = SpriteDecoyNotPlaying;
+        }
+    }
+}
