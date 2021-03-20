f3a578e6218a8dd65094c51ad359c5e85a8634a5
Hakej <michaluszak97@gmail.com>
2021-03-20 17:34:52 +0100

    Make SanityText more red with lower sanity

---
+++ Assets/SanityText.cs
@@ -0,0 +1,26 @@
+﻿using Classes.Static;
+using TMPro;
+using UnityEngine;
+
+public class SanityText : MonoBehaviour
+{
+    public TextMeshProUGUI Text;
+    private void Start()
+    {
+        InvokeRepeating("UpdateEverySecond", 0f, 1.0f);
+    }
+
+    private void UpdateEverySecond()
+    {
+        var currentSanity = SanityHandler.Instance.CurrentSanity;
+
+        if (SanityHandler.Instance.CurrentSanity > 50f)
+        {
+            Text.color = Color.white;
+        }
+        else
+        {
+            Text.color = Color.Lerp(Color.red, Color.white, currentSanity / 50f);
+        }
+    }
+}
