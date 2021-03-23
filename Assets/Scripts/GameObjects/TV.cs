caa52982e08819c839c71e4d88f7817c973ec570
Hakej <michaluszak97@gmail.com>
2021-03-23 21:37:54 +0100

    Add visual white noise rising with lower sanity

--- Assets/Scripts/GameObjects/TV.cs
+++ Assets/Scripts/GameObjects/TV.cs
@@ -1,22 +1,38 @@
 ﻿using Handlers;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TV : MonoBehaviour
 {
-    public AudioSource WhiteNoise;
     public TextMeshProUGUI SanityText;
-    
+
+    [Header("White Noise Hider")]
+    public Image WhiteNoiseHider;
+    public float MaxAlpha;
+    public float MinAlpha;
+
+
+    [Header("White Noise Sound")]
+    public AudioSource WhiteNoiseSource;
     public float MaxWhiteNoiseVolume = 0.1f;
     public float WhiteNoiseStartingSanity = 50f;
 
     private void Update()
     {
         var curSanity = GameHandler.Instance.SanityHandler.CurrentSanity;
-        
+
         SanityText.text = $"Sanity: {(int)curSanity}%";
-        
-        var noiseVolume = (WhiteNoiseStartingSanity - curSanity) / WhiteNoiseStartingSanity * MaxWhiteNoiseVolume;
-        WhiteNoise.volume = noiseVolume > 0 ? noiseVolume : 0f;
+
+        var noiseColor = WhiteNoiseHider.color;
+        var noiseValue = (WhiteNoiseStartingSanity - curSanity) / WhiteNoiseStartingSanity;
+
+        var noiseVolume = noiseValue > 0 ? noiseValue * MaxWhiteNoiseVolume : 0f;
+        var noiseAlpha = noiseValue > 0 ? MaxAlpha - noiseValue : MaxAlpha;
+
+        noiseColor.a = noiseAlpha;
+
+        WhiteNoiseHider.color = noiseColor;
+        WhiteNoiseSource.volume = noiseVolume;
     }
 }
