a2228069eccfe4c49f2f9ba3e2a14e2c80a7a09c
Hakej <michaluszak97@gmail.com>
2021-03-30 12:34:18 +0200

    Add Ambient and Red Herring handlers

---
+++ Assets/Scripts/AmbientSoundsHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Classes.Extensions;
+using UnityEngine;
+
+public class AmbientSoundsHandler : MonoBehaviour
+{
+
+    [Header("Cooldown")]
+    public float MinCooldown;
+    public float MaxCooldown;
+
+    private float _currentCooldown;
+    private AudioSource[] _redHerringAudioSources;
+
+    private void Start()
+    {
+        _currentCooldown = Random.Range(MinCooldown, MaxCooldown);
+
+        _redHerringAudioSources = GetComponentsInChildren<AudioSource>();
+    }
+
+    private void Update()
+    {
+        _currentCooldown -= Time.deltaTime;
+
+        if (_currentCooldown > 0f)
+        {
+            return;
+        }
+
+        var randomRedHerring = _redHerringAudioSources.GetRandomElement();
+
+        randomRedHerring.Play();
+
+        _currentCooldown = Random.Range(MinCooldown, MaxCooldown);
+    }
+}
