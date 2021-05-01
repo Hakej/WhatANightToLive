3ec936a5aa1179d71b9990fc09898b8234fa66ec
Hakej <michaluszak97@gmail.com>
2021-05-01 14:27:00 +0200

    Reduce max post processing weight for visual clarity

--- Assets/Scripts/InsanityPostProcessing.cs
+++ Assets/Scripts/InsanityPostProcessing.cs
@@ -7,6 +7,9 @@ public class InsanityPostProcessing : MonoBehaviour
     public float SanityThreshold = 50f;
     public Volume Volume;
 
+    public float MinimumWeight = 0f;
+    public float MaximumWeight = 0.2f;
+
     public SanityHandler SanityHandler;
 
     private void Update()
@@ -19,6 +22,6 @@ public class InsanityPostProcessing : MonoBehaviour
             return;
         }
 
-        Volume.weight = Mathf.Lerp(0.5f, 0f, currentSanity / 50f);
+        Volume.weight = Mathf.Lerp(MaximumWeight, MinimumWeight, currentSanity / 50f);
     }
 }
